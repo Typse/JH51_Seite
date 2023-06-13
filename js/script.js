21d84d1c705832c13ab945b0d3d086b97c67632c
Typse <36795023+Typse@users.noreply.github.com>
2023-06-13 07:37:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
+var navigation = document.getElementById('navigation');
+var moonText = document.getElementById('moon_text');
+var moonTextPosition = moonText.getBoundingClientRect().top;
+var screenPosition = window.innerHeight;
+
+
+window.addEventListener('scroll', function()
+{
+	if (window.pageYOffset === 0) 
+		{
+			navigation.classList.remove('hide');
+		}
+	else 
+		{
+			navigation.classList.add('hide');
+		}
+});
+
+window.addEventListener('scroll', function()
+{
+	if (moonTextPosition < screenPosition) 
+	{
+		moonText.classList.add('fade-in');
+	}
+});
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
 window.addEventListener('scroll', function() {
-    var nav = document.getElementById('navigation');
-    if (window.pageYOffset === 0) {
-        nav.classList.remove('hide');
-    } else {
-        nav.classList.add('hide');
+    var moonText = document.getElementById('moon_text');
+
+    if (isInViewport(moonText)) {
+        moonText.classList.add('fade-in');
     }
 });
